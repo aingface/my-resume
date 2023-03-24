f18f1e5f1dcf6176423107616f93395cb2458f41
배대철 <qoeocjf5@gmail.com>
2023-03-24 18:45:51 +0900

    add: 다른 경험 추가

--- resume/src/items/Skills.tsx
+++ resume/src/items/Skills.tsx
@@ -5,10 +5,6 @@ function Skills() {
     <div className="Chapter">
       <h2 className="ExperienceTitle">Skills</h2>
       <br />
-      <h3 className="ItemTitle">한국외국어대학교 글로벌캠퍼스</h3>
-      <p>태국어통번역학과</p>
-      <p>AI융합학부(AI&Software)</p>
-      <p>2014. 03 - 2022.02</p>
     </div>
   );
 }
