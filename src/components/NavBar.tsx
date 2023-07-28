cbf8ab9028144bd611e94fd861d2976805138097
배대철 <qoeocjf5@gmail.com>
2023-07-28 18:51:28 +0900

    Add: 스크롤 시 NavBar 배경 색 변경
    - 다크모드 배경 색인 #1e2127에서 투명도 30%인 rgba(30, 33, 39, 0.3)으로 변경

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/NavBar.module.css';
 import DarkModeToggle from '../components/DarkModeToggle';
 import { Link } from 'react-scroll';
 
-const NavBar: React.FC = () => {
+function NavBar() {
+  const [scrolling, setScrolling] = useState<boolean>(false);
+  useEffect(() => {
+    // 스크롤 이벤트 핸들러 등록
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      // 스크롤 위치가 0보다 크면(스크롤이 발생하면) 배경 색 변경
+      setScrolling(scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // 컴포넌트 언마운트 시 이벤트 핸들러 제거
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.Container}>
+    <div className={`${styles.Container} ${scrolling ? styles.scrolling : ''}`}>
       <div className={styles.MenuWrapper}>
-        <Link to="introduce" smooth={true} duration={500}>
+        <Link to="introduce" smooth={true} duration={0}>
           <div>Introduce</div>
         </Link>
-        <Link to="projectExperience" smooth={true} duration={500}>
+        <Link to="projectExperience" smooth={true} duration={0}>
           <div>Project Experience</div>
         </Link>
-        <Link to="otherExperience" smooth={true} duration={500}>
+        <Link to="otherExperience" smooth={true} duration={0}>
           <div>Other Experience</div>
         </Link>
       </div>
       <DarkModeToggle />
     </div>
   );
-};
+}
 
 export default NavBar;
